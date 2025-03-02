980c6064cfe252aed2c892f50f54644af2ae55f6
gsk-007 <gsingh16742@gmail.com>
2025-03-02 21:41:29 +0530

    add error handling for env variables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
-import app from './server'
+import app from "./server";
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`hello on http://localhost:${port}`)
-})
+const start = () => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL must be defined");
+  }
+  if (!process.env.REDIS_URL) {
+    throw new Error("REDIS_URL must be defined");
+  }
+  app.listen(port, () => {
+    console.log(`hello on http://localhost:${port}`);
+  });
+};
+
+start();
